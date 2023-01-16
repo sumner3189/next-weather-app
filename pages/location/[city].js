4e0463aed57e80d2acee666c59260d280905abb7
sumner3189 <donidsumner2017@gmail.com>
2023-01-16 08:24:33 -0800

    updated

---
+++ pages/location/[city].js
@@ -0,0 +1,19 @@
+import LocationSearchBar from "../../components/LocationSearchBar";
+import Link from "next/link";
+
+export default function City() {
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="d-flex justify-content-center">
+          <p className="w-50">
+            <Link href={"/"}>&larr; Home</Link>
+          </p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <LocationSearchBar className="text-center" />
+        </div>
+      </div>
+    </>
+  );
+}
